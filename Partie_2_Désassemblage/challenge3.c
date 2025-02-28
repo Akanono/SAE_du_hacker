fa9a94fa1c98a981d0632ea387cb86846cef7fd5
Akanono <akanomey@gmail.com>
2025-02-28 12:39:40 +0100

    Update challenge3.c

--- Partie_2_Désassemblage/challenge3.c
+++ Partie_2_Désassemblage/challenge3.c
@@ -2,20 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Missing login and password !\n");
+        return 1;
+    }
+
     char correct_login[] = "XahHaf1J";
     char correct_password[] = "Fae9v";
-    char input_login[50], input_password[50];
 
-    printf("Login : ");
-    scanf("%s", input_login);
-    printf("Password : ");
-    scanf("%s", input_password);
-
-    if (strcmp(input_login, correct_login) == 0 && strcmp(input_password, correct_password) == 0) {
-        printf("** ACCESS GRANTED **\n");
+    if (strcmp(argv[1], correct_login) == 0 && strcmp(argv[2], correct_password) == 0) {
+        printf(" ACCESS GRANTED \n");
     } else {
-        printf("** ACCESS DENIED **\n");
+        printf(" ACCESS DENIED \n");
     }
     return 0;
 }
