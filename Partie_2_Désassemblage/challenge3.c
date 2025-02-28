387a3aae475b0e215b6a8753c5722313d7a21070
Akanono <akanomey@gmail.com>
2025-02-28 12:22:31 +0100

    Create challenge3.c

---
+++ Partie_2_Désassemblage/challenge3.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char correct_login[] = "XahHaf1J";
+    char correct_password[] = "Fae9v";
+    char input_login[50], input_password[50];
+
+    printf("Login : ");
+    scanf("%s", input_login);
+    printf("Password : ");
+    scanf("%s", input_password);
+
+    if (strcmp(input_login, correct_login) == 0 && strcmp(input_password, correct_password) == 0) {
+        printf("** ACCESS GRANTED **\n");
+    } else {
+        printf("** ACCESS DENIED **\n");
+    }
+    return 0;
+}
