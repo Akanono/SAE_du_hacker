b254a44d0340bd3a07da09892f87ccdfa6a70d83
Lerazoo <yannickwan1@gmail.com>
2025-03-01 19:23:24 +0100

    Create s_string.h

---
+++ Partie_1_Super_LibC/include/s_string.h
@@ -0,0 +1,16 @@
+#ifndef S_STRING_H
+#define S_STRING_H
+
+#include <stddef.h>  // Pour size_t
+
+// Déclarations des fonctions
+extern int s_strlen(const char *str);
+extern char *s_strcpy(char *dest, const char *src);
+extern char *s_strncpy(char *dest, const char *src, size_t n);
+extern char *s_strcat(char *dest, const char *src);
+extern char *s_strncat(char *dest, const char *src, size_t n);
+extern int s_strcmp(const char *s1, const char *s2);
+extern int s_strncmp(const char *s1, const char *s2, size_t n);
+extern char *s_strchr(const char *s, int c);
+
+#endif // S_STRING_H
